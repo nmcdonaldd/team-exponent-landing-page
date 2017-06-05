913de980999ba382e1722252e252fca5a30c777c
Sam Harms <samharms@Sams-MacBook-Pro-3.local>
2017-06-04 18:59:20 -0700

    js and clean-up

---
+++ landing_page/app/static/js/profile.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  function myFunction() {
+      var x = document.getElementById('p');
+      if (x.style.visibility == 'hidden') {
+          x.style.visibility = 'visible';
+      } else {
+          x.style.visibility = 'hidden';
+      }
+  }
+
+  function myFunction2() {
+      var x = document.getElementById('p2');
+      if (x.style.visibility == 'hidden') {
+          x.style.visibility = 'visible';
+      } else {
+          x.style.visibility = 'hidden';
+      }
+  }
+
+});
